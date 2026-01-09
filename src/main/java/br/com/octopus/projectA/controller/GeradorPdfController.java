01877bd750ce671d72a0c9dd5cd4d0cb7d1f606c
Julio Cesar <jcesar.s.machado@gmail.com>
2026-01-08 21:34:54 -0300

    fix: Fixed feature for generating payment statements in PDF format.

--- src/main/java/br/com/octopus/projectA/controller/GeradorPdfController.java
+++ src/main/java/br/com/octopus/projectA/controller/GeradorPdfController.java
@@ -6,6 +6,9 @@ import br.com.octopus.projectA.suport.dtos.DadosFolhaPagementoDTO;
 import br.com.octopus.projectA.suport.dtos.RequestFolhaPagamentoDTO;
 import com.itextpdf.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,9 +25,24 @@ public class GeradorPdfController {
 
     private final String TEMPLATE_FOLHA_PAGAMENTO = "templatesHTML/folhaPagamento";
 
-    @PostMapping("/pdfDecisaoSemAssinatura")
-    public ResponseEntity<byte[]> gerarPdfDecisaoSemAssinatura(@RequestBody RequestFolhaPagamentoDTO requestFolhaPagamentoDTO) throws DocumentException, IOException {
-        return ResponseEntity.ok(geradorPdfService.gerarFolhaPagemnto(TEMPLATE_FOLHA_PAGAMENTO, requestFolhaPagamentoDTO));
+    @PostMapping("/gerar-pdf-folha-de-pagamento")
+    public ResponseEntity<byte[]> gerarPdfFolhaDePagamento(@RequestBody RequestFolhaPagamentoDTO requestFolhaPagamentoDTO) throws DocumentException, IOException {
+        try {
+            byte[] pdfBytes = geradorPdfService.gerarFolhaPagemnto(TEMPLATE_FOLHA_PAGAMENTO, requestFolhaPagamentoDTO);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF); // MediaType.APPLICATION_PDF é "application/pdf"
+            String filename = "folha_pagamento_" + requestFolhaPagamentoDTO.getIdAgente() + ".pdf";
+            headers.setContentDispositionFormData(filename, filename);
+            headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+
+            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
 
     @PostMapping("/teste")
